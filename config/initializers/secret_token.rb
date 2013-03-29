087ff064b714a5f3ccbb7bcc305b370c4aca3a1d
ddisabatino <ddisabatino92@gmail.com>
2013-03-28 22:12:09 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dpdlolcats::Application.config.secret_token = 'e962b51370b4fadb421b0946bb944ac86f762d42e66846a618b35fc0e9090e28db3db985f74bfba443a964f772ed35193803ea7c5e2bf13daaa8182c58c358a0'
